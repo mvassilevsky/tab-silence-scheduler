275cb143280a18d3b6e49af3a0516152b70c95f4
Michael Vassilevsky <mvassilevsky@gmail.com>
2020-04-29 16:51:09 -0700

    Mute tabs at scheduled times

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
-chrome.alarms.onAlarm.addListener(function( alarm ) {
-  console.log("Got an alarm!", alarm);
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  chrome.tabs.query({}, function(tabList) {
+    tabList.forEach(function(tab) {
+      updateTab(tab, alarm);
+    });
+  });
 });
+
+function updateTab(tab, alarm) {
+  if (alarm['name'] == 'muteStart') {
+    chrome.tabs.update(tab.id, { muted: true } );
+  } else if (alarm['name'] == 'muteEnd') {
+    chrome.tabs.update(tab.id, { muted: false } );
+  }
+}
